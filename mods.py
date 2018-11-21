10ae5cac2720f7e0a4654f20eeb734a0328971aa
Tahleia Mascord <38026867+Octopotatopuss@users.noreply.github.com>
2018-11-22 09:45:07 +1100

    Arrays
    From last lesson

--- mods.py
+++ mods.py
@@ -1,12 +1,14 @@
-""" Project name.
+""" Modules of Standard Algorithms functions for the SDD course 
 
-Description
+this script will contain functions corresponding to all the standard
+algorithms listed in the course specs for the HSC SDD course
+
 """
 
-__author__ = "Your Name"
+__author__ = "Tahleia Mascord"
 __license__ = "GPL"
 __version__ = "1.0.1"
-__email__ = "your.address@education.nsw.com.au"
+__email__ = "Tahleia.Mascord1@education.nsw.com.au"
 __status__ = "Prototype, Development or Production"
 
 """ revision notes:
@@ -17,10 +19,59 @@ __status__ = "Prototype, Development or Production"
 #include statements
 
 
-def function_name(arg1, arg2, other_silly_variable=None):
-    """Does something amazing.
+def loadArray():
+    """load data to an array.
 
-    a much longer description of the really amazing stuff this function does and how it does it.
+    prompts the user to enter data and
+    loads that data into an array.
+
+
+    Args:
+
+    Returns:
+        
+    Raises:
+        
+    """
+    thisArray = []
+    i = 0
+    dataValue = input("please enter some data, or 'xxx' to quit: ")
+    
+    while dataValue != 'xxx' :
+        thisArray[i] = dataValue
+        i = i + 1
+        dataValue = input("please enter some data, or 'xxx' to quit: ")
+            
+        numElements = i
+        print("There are " + str(numElements) + "items in the array")
+        
+        
+
+""""class SampleClass(object):
+       Summary of class here.
+
+    Longer class information....
+
+    Attributes:
+        likes_spam: A boolean indicating if we like SPAM or not.
+        eggs: An integer count of the eggs we have laid.
+    """
+
+    def __init__(self, likes_spam=False):
+        Inits SampleClass with blah.
+        self.likes_spam = likes_spam
+        self.eggs = 0
+
+    def public_method(self):
+        Performs operation blah."""
+
+
+#copy of template: 
+
+""""""def function_name(arg1, arg2, other_silly_variable=None):
+    Does something amazing.
+
+    "a much longer description of the really amazing stuff this function does and how it does it.
 
     Args:
         arg1: the first argument required by the function.
@@ -34,25 +85,5 @@ def function_name(arg1, arg2, other_silly_variable=None):
     Raises:
         AnError: An error occurred running this function.
     """
-    pass
-
-class SampleClass(object):
-    """Summary of class here.
-
-    Longer class information....
-
-    Attributes:
-        likes_spam: A boolean indicating if we like SPAM or not.
-        eggs: An integer count of the eggs we have laid.
-    """
-
-    def __init__(self, likes_spam=False):
-        """Inits SampleClass with blah."""
-        self.likes_spam = likes_spam
-        self.eggs = 0
-
-    def public_method(self):
-        """Performs operation blah."""
-
-
+    
 
